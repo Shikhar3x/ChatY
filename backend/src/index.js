1ba1ce044467661fd64927102423b1e8e51ca2db
Shikhar <shikharx.dev@gmail.com>
2026-01-22 10:35:06 +0530

    system-cooked

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,15 +8,18 @@ import cors from "cors";
 import { app, server } from "./utils/socket.js";
 
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const __dir = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -28,15 +31,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
 connectDB().then(() => {
   server.listen(PORT, () => {
-    console.log("Server is Running in PORT " + PORT);
+    console.log("✅ Server running on port " + PORT);
   });
 });
